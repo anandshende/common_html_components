03614942d0d249a1f099f1868fc8c91242b16c7d
ANAND\anand_000 <anandshende1994@gmail.com>
2019-01-28 16:14:51 +0530

    Setting pitch and volume

--- text-to-speech/web-speech-synthesis-api/scripts/index.js
+++ text-to-speech/web-speech-synthesis-api/scripts/index.js
@@ -3,6 +3,10 @@ var speakWord = function (event) {
     var word = target.innerText;
     
     var voiceProperties = new SpeechSynthesisUtterance(word);
+    voiceProperties.volume = 1;
+    voiceProperties.pitch = 0;
+    voiceProperties.lang = 'en-EN';
+    
     var synth = window.speechSynthesis;
     synth.speak(voiceProperties);
 };
